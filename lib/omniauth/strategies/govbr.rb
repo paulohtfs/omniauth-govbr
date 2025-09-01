9dc28745b321a9821070c44460c80fd4198b2577
Paulo Tada <paulohtfs@gmail.com>
2025-09-01 11:13:25 -0300

    fix: redo nickname to use name instead of sub

--- lib/omniauth/strategies/govbr.rb
+++ lib/omniauth/strategies/govbr.rb
@@ -21,8 +21,9 @@ module OmniAuth
         {
           sub: raw_info['sub'],
           name: raw_info['name'],
-          nickname: raw_info['sub'],
-          email: raw_info['email']
+          nickname: nickname,
+          email: raw_info['email'],
+          locale: raw_info['locale']
         }
       end
 
@@ -31,11 +32,15 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get(options.client_options['user_info_url']).parsed || {}
+        @_raw_info ||= access_token.get(options.client_options['user_info_url']).parsed || {}
       end
 
       def callback_url
         full_host + callback_path
+      end
+
+      def nickname
+        @_nickname ||= raw_info['name'].gsub(' ', '_').downcase
       end
     end
   end
